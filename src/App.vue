edb5973fdd194475c2fd86724952a0c9da6d3b75
shubhamkshatriya25 <shubhamkshatriya521@gmail.com>
2023-01-21 23:08:08 +0530

    Created base layout

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app class="app">
+    <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :options="particlesOptions"
+    />
+    <v-main>
+      <IndexScreen />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import IndexScreen from "./components/IndexScreen";
+import { loadFull } from "tsparticles";
+import { particlesJSConfig } from "./assets/configs/particlesConfig";
+
+export default {
+  name: "App",
+  components: {
+    IndexScreen,
+  },
+  data() {
+    return {
+      particlesOptions: particlesJSConfig,
+    };
+  },
+  methods: {
+    particlesInit: async function (engine) {
+      await loadFull(engine);
+    },
+    particlesLoaded: async function (container) {
+      console.log("Particles container loaded", container);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.app {
+  background-color: #232741 !important;
+  color: #31302c !important;
+  font-family: Jost, sans-serif;
+  font-size: 15px;
+  margin: 0;
+  height: 100%;
+}
+
+#tsparticles {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Particles from "vue2-particles";
+
+Vue.config.productionTip = false;
+Vue.use(Particles);
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
